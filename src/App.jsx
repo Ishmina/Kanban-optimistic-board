dd22e0547fbc0360ee4a1d57c16f9745e37af887
Ishu <ishminasultana@gmail.com>
2026-01-22 16:41:02 +0530

    chore: initialize React + Vite project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+
+      <div className="min-h-screen bg-pink-300 p-6">
+      <h1 className="text-2xl font-semibold">
+        Kanban Board
+      </h1>
+    </div>
+
+  )
+}
+
+export default App
